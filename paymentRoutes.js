b5798f60e15b837cc4ad0c6a77863e179ced21d8
descmd1 <aondoakaachris@gmail.co>
2025-06-25 09:18:26 +0100

    edited payment verification

--- paymentRoutes.js
+++ paymentRoutes.js
@@ -26,7 +26,7 @@ router.post('/buycourse', async (req, res) => {
             {
                 email,
                 amount: amount * 100, // Amount should be in kobo
-                callback_url: `http://localhost:3000/verifypayment/${courseId}`, // Redirect after payment
+                callback_url: `https://lms-api-pr4i.onrender.com/verifypayment/${courseId}`, // Redirect after payment
                 metadata: {
                     courseId: courseId,
                     userId: userId
